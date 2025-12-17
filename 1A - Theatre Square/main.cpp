4544cba92ff4c0a79aaa381ae22b3e8df4c74f81
Maxi <jeezmax17@gmail.com>
2025-12-17 15:19:32 -0300

    nothing

--- 1A - Theatre Square/main.cpp
+++ 1A - Theatre Square/main.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(){
     long long n, m, a; 
     cin >> n >> m >> a;
     
-    long long r1 = (n + a - 1) / a;
-    long long r2 = (m + a - 1) / a;
-    long long result = r1 * r2;
+    ll r1 = (n + a - 1) / a;
+    ll r2 = (m + a - 1) / a;
+    ll result = r1 * r2;
 
     cout << result << endl;
 
